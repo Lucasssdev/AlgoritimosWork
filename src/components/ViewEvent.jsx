1564b4d1fe5ff6790f61501b00872a1a64c3d823
LucasLemos <lucaslfarah25@gmail.com>
2022-11-17 10:02:50 -0300

    ginish fucnional

--- src/components/ViewEvent.jsx
+++ src/components/ViewEvent.jsx
@@ -4,8 +4,8 @@ export default function ViewEvent({historyPlayers, historyEvent}){
     return(
        <ul>
         {
-            historyEvent.lenth != [] 
-            ? historyEvent.map((event) =>  <AllEvents historyPlayers={historyPlayers} description={event.description} local={event.local} id={event.id}/>) 
+            historyEvent!= [] 
+            ? historyEvent.map((event,i=0) =>  <AllEvents key={i++} historyPlayers={historyPlayers} description={event.description} local={event.local} id={event.id}/>) 
             : <h1>Nao Ha eventos</h1>
         }
        </ul>
